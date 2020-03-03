1ed7a2c481ae2ad41ada803f08354a552902e98b
Alexandre Zajac <zajac.alexandre@gmail.com>
2020-03-03 17:32:55 +0100

    feat(tests): Validate tests with Jest

--- currency/index.test.js
+++ currency/index.test.js
@@ -1,76 +1,135 @@
-const nock = require('nock');
+const nock = require("nock");
+const currency = require("./");
 
+// Constants stubs for tests
+const BASE_ERROR =
+  "💵 Please specify a valid `from` and/or `to` currency value!";
+const USD_EUR = 0.899;
+const EUR_EUR = 1;
+const EUR_USD = 1.1122;
+const BTC_USD = 8944.49;
+const BTC_EUR = 8048.11;
+
+// nock provides mocking for HTTP requests, here we mock received exchange rates to constant values for testing
 beforeEach(() => {
-  nock('https://api.exchangeratesapi.io')
-    .get('/latest?base=USD')
+  nock("https://api.exchangeratesapi.io")
+    .get("/latest?base=USD")
     .reply(200, {
-      'base': 'USD',
-      'rates': {
-        'EUR': 0.899
+      base: "USD",
+      rates: {
+        EUR: USD_EUR
       }
     });
 
-  nock('https://api.exchangeratesapi.io')
-    .get('/latest?base=EUR')
+  nock("https://api.exchangeratesapi.io")
+    .get("/latest?base=EUR")
     .reply(200, {
-      'base': 'EUR',
-      'rates': {
-        'USD': 1.1122
+      base: "EUR",
+      rates: {
+        USD: EUR_USD
       }
     });
 
-  nock('https://blockchain.info')
-    .get('/ticker')
+  nock("https://blockchain.info")
+    .get("/ticker")
     .reply(200, {
-      'USD': {
-        '15m': 8944.49,
-        'last': 8944.49,
-        'buy': 8944.49,
-        'sell': 8944.49,
-        'symbol': '$'
+      USD: {
+        "15m": BTC_USD,
+        last: BTC_USD,
+        buy: BTC_USD,
+        sell: BTC_USD,
+        symbol: "$"
       },
-      'EUR': {
-        '15m': 8048.11,
-        'last': 8048.11,
-        'buy': 8048.11,
-        'sell': 8048.11,
-        'symbol': '€'
+      EUR: {
+        "15m": BTC_EUR,
+        last: BTC_EUR,
+        buy: BTC_EUR,
+        sell: BTC_EUR,
+        symbol: "€"
       }
     });
 });
 
-test('convert 1 USD to EUR', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+/* JEST TESTS
+   Each test follows the same structure: 
+   - Arrange the data for the given test
+   - Act, get the mocked value from the API
+   - Assert, compare them with constants previously defined
+*/
+
+test("convert 1 USD to EUR", async () => {
+  const amount = 1,
+    from = "USD",
+    to = "EUR";
+  const opts = { amount, from, to };
+  const result = await currency(opts);
+  expect(result).toBe(USD_EUR);
 });
 
-test('convert 1 USD to USD', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+test("convert 1 USD to USD", async () => {
+  const amount = 1,
+    from = "EUR",
+    to = "EUR";
+  const opts = { amount, from, to };
+  const result = await currency(opts);
+  expect(result).toBe(EUR_EUR);
 });
 
-test('convert 1 EUR to USD', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+test("convert 1 EUR to USD", async () => {
+  const amount = 1,
+    to = "USD",
+    from = "EUR";
+  const opts = { amount, from, to };
+  const result = await currency(opts);
+  expect(result).toBe(EUR_USD);
 });
 
-test('convert 1 BTC to USD', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+test("convert 1 BTC to USD", async () => {
+  const amount = 1,
+    from = "BTC",
+    to = "USD";
+  const opts = { amount, from, to };
+  const result = await currency(opts);
+  expect(result).toBe(BTC_USD);
 });
 
-test('convert 1 BTC to EUR', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+test("convert 1 BTC to EUR", async () => {
+  const amount = 1,
+    from = "BTC",
+    to = "EUR";
+  const opts = { amount, from, to };
+  const result = await currency(opts);
+  expect(result).toBe(BTC_EUR);
 });
 
-test('convert without arguments', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+test("convert without arguments", async () => {
+  // here the default is 1 USD to BTC
+  const opts = {};
+  const result = await currency(opts);
+  expect(result).toBe(1 / BTC_USD);
 });
 
-test('convert with amount only', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+test("convert with amount only", async () => {
+  // here the default is 1 USD to BTC
+  const amount = 3;
+  const opts = { amount };
+  const result = await currency(opts);
+  expect(result).toBe(amount / BTC_USD);
 });
 
-test('convert with amount and (from) currency only', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+test("convert with amount and (from) currency only", async () => {
+  // here the default is 1 USD to BTC
+  const amount = 4,
+    from = "USD";
+  const opts = { amount, from };
+  const result = await currency(opts);
+  expect(result).toBe(amount / BTC_USD);
 });
 
-test('convert without a correct `from` or `to` currency value', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+test("convert without a correct `from` or `to` currency value", () => {
+  const amount = 1,
+    from = "LIBRA",
+    to = "CORONACOIN";
+  const opts = { amount, from, to };
+  return currency(opts).catch(e => expect(e.message).toMatch(BASE_ERROR));
 });
